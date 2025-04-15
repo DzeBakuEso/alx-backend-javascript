d4bf7a0e937d6e565ba9faf7a112520446aade9b
DzeBakuEso <dzeblebaku@gmail.com>
2025-04-15 08:50:42 -1200

    0-constants.js

--- 0x00-ES6_basics/.eslintrc.js
+++ 0x00-ES6_basics/.eslintrc.js
@@ -2,11 +2,11 @@ module.exports = {
   env: {
     browser: false,
     es6: true,
-    // jest: true, // ❌ Removed to fix lint crash
+    jest: true,
   },
   extends: [
     'airbnb-base',
-    // 'plugin:jest/all', // ❌ Removed conflicting plugin
+    'plugin:jest/all',
   ],
   globals: {
     Atomics: 'readonly',
@@ -16,7 +16,7 @@ module.exports = {
     ecmaVersion: 2018,
     sourceType: 'module',
   },
-  // plugins: ['jest'], // ❌ Removed conflicting plugin
+  plugins: ['jest'],
   rules: {
     'no-console': 'off',
     'no-shadow': 'off',
@@ -26,10 +26,10 @@ module.exports = {
       'WithStatement',
     ],
   },
-  overrides: [
+  overrides:[
     {
       files: ['*.js'],
       excludedFiles: 'babel.config.js',
     }
-  ],
+  ]
 };
