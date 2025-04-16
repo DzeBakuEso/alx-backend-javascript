e76c74d213fa8e0c9df569c00e64758359a56451
DzeBakuEso <dzeblebaku@gmail.com>
2025-04-16 08:47:42 -1200

    8-try.js

---
+++ 0x01-ES6_promise/8-main.js
@@ -0,0 +1,8 @@
+5
+/home/user/path/8-try.js:3
+  throw new Error('cannot divide by 0');
+        ^
+
+Error: cannot divide by 0
+    at divideFunction (/path/8-try.js:3:11)
+    at Object.<anonymous> (/path/8-main.js:4:13)
