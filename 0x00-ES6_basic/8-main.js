48a1c5a4c34b7e4906b357c192e29bcac92aec51
DzeBakuEso <dzeblebaku@gmail.com>
2025-04-17 22:29:03 -1200

    8-getBudgetCurrentYear.js

---
+++ 0x00-ES6_basic/8-main.js
@@ -0,0 +1,5 @@
+import getBudgetForCurrentYear from './8-getBudgetCurrentYear.js';
+
+console.log(getBudgetForCurrentYear(2100, 5200, 1090));
+// Expected Output (year will match current year):
+// { 'income-2025': 2100, 'gdp-2025': 5200, 'capita-2025': 1090 }
