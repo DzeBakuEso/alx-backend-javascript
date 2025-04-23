6632e696a1f8936c04aeb8bc0d0bffcbd4b9a753
DzeBakuEso <dzeblebaku@gmail.com>
2025-04-23 08:05:36 -1200

    task_1/js/main.ts

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,18 +1,25 @@
+// Define the Teacher interface
 interface Teacher {
   readonly firstName: string;
   readonly lastName: string;
   fullTimeEmployee: boolean;
   yearsOfExperience?: number;
   location: string;
-  [propName: string]: any; // index signature for additional attributes
+  [key: string]: any; // Allow additional dynamic properties
 }
 
-const teacher3: Teacher = {
+// Extend Teacher to define Directors interface
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+// Example object of type Directors
+const director1: Directors = {
   firstName: 'John',
   lastName: 'Doe',
-  fullTimeEmployee: false,
   location: 'London',
-  contract: false,
+  fullTimeEmployee: true,
+  numberOfReports: 17,
 };
 
-console.log(teacher3);
+console.log(director1);
