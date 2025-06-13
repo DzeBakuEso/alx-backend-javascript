27c9f76e1c0a1326e1c04d9db620f068f666fa9c
DzeBaKuEso <dzeblebaku@gmail.com>
2025-06-12 23:00:47 -1200

    update

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,17 +1,20 @@
-const chai = require('chai');
-const chaiHttp = require('chai-http');
-const app = require('./api');
+const request = require('request');
+const { expect } = require('chai');
 
-chai.use(chaiHttp);
-const { expect } = chai;
+describe('Index page', () => {
+  const url = 'http://localhost:7865/';
 
-describe('GET /', () => {
-  it('should return status 200', (done) => {
-    chai.request(app)
-      .get('/')
-      .end((err, res) => {
-        expect(res).to.have.status(200);
-        done();
-      });
+  it('Correct status code?', (done) => {
+    request.get(url, (err, res, body) => {
+      expect(res.statusCode).to.equal(200);
+      done();
+    });
+  });
+
+  it('Correct result?', (done) => {
+    request.get(url, (err, res, body) => {
+      expect(body).to.equal('Welcome to the payment system');
+      done();
+    });
   });
 });
